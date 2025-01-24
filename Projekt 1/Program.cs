60093c4ec18c531a87df795b20f06664eac237fc
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2025-01-24 10:51:53 +0100

    Kordinat system funkar och i en funktion

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -1,66 +1,71 @@
 ﻿Random rnd = new Random();
-System.Console.WriteLine("Hej Så Du Har valt Att Utforska Den Gamla Labyrinten Och Leta Efter Guldet Som Är Gömt här");
-System.Console.WriteLine("Vad Är Ditt Namn?");
+System.Console.WriteLine("Hej, Så Du Har valt Att Utforska Den Gamla Labyrinten Och Leta Efter Guldet Som Är Gömt här");
+System.Console.WriteLine("Vad Är Ditt Namn Eller Smeknamn?");
 string namn = Console.ReadLine();
 while (namn == "")
-{  
+{
     // bara så att man inte skriver ingenting som namnet
     System.Console.WriteLine("Det Är Inte Ett Namn");
     System.Console.WriteLine("Skriv Ett Namn");
     namn = Console.ReadLine();
 }
 System.Console.WriteLine("Hej " + namn + " Lycka Till I Labyrinten");
-bool k = false;
-
-int XAxelnr = rnd.Next(1, 6);
-int YAxelnr  = rnd.Next(1, 7);
-List<int> XAxel = new List<int>();
-List<int> YAxel = new List<int>();
-while (XAxel.Count < 6)
-{
-    while (true)
-    {
-
-        for (int i = 0; i < XAxel.Count; i++)
-        {
-            XAxelnr = rnd.Next(1, 6);
-            if (XAxelnr == XAxel[i])
-            {
-                k = false;
-                break;
-            }
-            k = true;
-        }
-        if (k == true)
-        {
-            break;
-        }
-    }
-    XAxel.Add(XAxelnr);
-}
+int cordinat = 5;
+List<string> xAxel = new List<string>();
+List<string> yAxel = new List<string>();
+xAxel = Cordinater(cordinat);
+cordinat = 6; 
+yAxel = Cordinater(cordinat);
 
 
-while (YAxel.Count < 7)
-{
-    while (true)
-    {
 
-        for (int i = 0; i < YAxel.Count; i++)
-        {
-            YAxelnr = rnd.Next(1, 7);
-            if (YAxelnr == YAxel[i])
-            {
-                k = false;
-                break;
-            }
-            k = true;
-        }
-        if (k == true)
-        {
-            break;
-        }
-    }
-    YAxel.Add(YAxelnr);
-}
+
 Console.ReadLine();
-hej
+
+static List<string> Cordinater(int cordinat)
+{
+    Random rnd = new Random();
+    bool k = false;
+    string xAxelNr = rnd.Next(1, cordinat).ToString();
+    List<string> xAxel = new List<string>();
+    xAxel.Add("1");
+    while (xAxel.Count < cordinat)
+    {
+        while (true)
+        {
+            xAxelNr = rnd.Next(1, cordinat+1).ToString();
+            for (int i = 0; i < xAxel.Count; i++)
+            {
+                if (xAxel.Count > cordinat-1)
+                {
+                    k = true;
+                    break;
+                }
+                k = true;
+                if (xAxelNr == xAxel[i])
+                {
+                    k = false;
+                    break;
+                }
+            }
+            if (k == true)
+            {
+                break;
+            }
+        }
+        xAxel.Add(xAxelNr);
+    }
+    return xAxel;
+}
+
+void val()
+{
+    if (x = xAxel[1])
+    {
+        System.Console.WriteLine("Du kan gå höger,");
+    }
+    else if( x = xAxel[5])
+    {
+        System.Console.WriteLine("Du kan gå vänster,");
+    }
+}
